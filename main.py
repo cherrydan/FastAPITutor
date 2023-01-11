ff75c70cbf94e944f50f41e8262cc7b7cc4f04d5
cherrydan <cherrydan.1978@gmail.com>
2023-01-11 11:38:35 +0300

    Adds settings and main script

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from fastapi import FastAPI
+import uvicorn
+from fastapi.routing import APIRouter
+from sqlalchemy import Column, Boolean, String
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+import settings
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+import re
+from fastapi import HTTPException
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import validator
+
+
+if __name__ == '__main__':
+    pass
+
 
