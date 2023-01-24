ab85e9d7bb2006ef7e8adf767ae3cbfa7a178977
cherrydan <cherrydan.1978@gmail.com>
2023-01-24 23:50:36 +0300

    adds automatically creation db on app startup

--- main.py
+++ main.py
@@ -130,6 +130,14 @@ main_api_router = APIRouter()
 main_api_router.include_router(user_router, prefix='/user', tags=["user"])
 app.include_router(main_api_router)
 
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
 if __name__ == '__main__':
     # run uvicorn server on host & port
     uvicorn.run(app, host="0.0.0.0", port=8000)
