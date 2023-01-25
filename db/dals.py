e4e5996a1641e3f73aaf7a125825e4619e9288ac
cherrydan <cherrydan.1978@gmail.com>
2023-01-25 18:26:19 +0300

    rewrite project structure

---
+++ db/dals.py
@@ -0,0 +1,22 @@
+###########################################################
+# BLOCK FOR INTERACTION WITH DATABASE IN BUSINESS CONTEXT #              #
+###########################################################
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models import User
+
+
+class UserDAL:
+    """
+    User data-access layer for operating user info
+    """
+
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_user(self,
+                          name: str, surname: str, email: str) -> User:
+        new_user = User(name=name, surname=surname, email=email)
+        self.db_session.add(new_user)
+        await self.db_session.flush()
+        return new_user
