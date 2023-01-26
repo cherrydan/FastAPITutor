0b802ce70e3e97fedffc1d7ec4612fd6c47ab85f
cherrydan <cherrydan.1978@gmail.com>
2023-01-26 20:06:18 +0300

    make creating table users on app startup

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ main_api_router.include_router(user_router, prefix='/user', tags=["user"])
 app.include_router(main_api_router)
 
 
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
 if __name__ == '__main__':
     # run uvicorn server on host & port
     uvicorn.run(app, host="0.0.0.0", port=8000)
