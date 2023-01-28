31eb20c76af96af9c4551496c63aefa0fbc19aab
cherrydan <cherrydan.1978@gmail.com>
2023-01-28 13:13:48 +0300

    complete writing fixtures for tests, not run

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,7 +40,7 @@ async def run_migrations():
 def async_session_test():
     engine = create_async_engine(TEST_DATABASE_URL, future=True, echo=True)
     async_session = sessionmaker(engine, expire_on_commit=True, class_=AsyncSession)
-    yield AsyncSession
+    yield async_session
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -49,3 +49,33 @@ async def clean_tables(async_session_test):
         async with session.begin():
             for table_for_cleaning in CLEAN_TABLES:
                 await session.execute(f"""TRUNCATE {table_for_cleaning};""")
+
+
+@pytest.fixture(scope="function")
+async def client() -> Generator[TestClient, Any, None]:
+    async def _get_test_db():
+        try:
+            yield test_async_session()
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = _get_test_db()
+
+    with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture(scope="session")
+async def asyncpg_pool():
+    pool = await asyncpg.create_pool("".join(TEST_DATABASE_URL.split("+asyncpg")))
+    yield pool
+    pool.close()
+
+
+@pytest.fixture
+async def get_user_from_database(asyncpg_pool):
+    async def get_user_from_database_by_uuid(user_id: str):
+        async with asyncpg_pool.acquire() as conn:
+            return await conn.fetch("""SELECT FROM users WHERE user_id = $1;""", user_id)
+
+    return get_user_from_database_by_uuid
