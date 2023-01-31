aa598f131985e15325fa78a21329fac30b45e432
cherrydan <cherrydan.1978@gmail.com>
2023-01-31 21:58:28 +0300

    Correct many mistakes in test_handlers and test passed!

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -16,11 +16,11 @@ async def test_create_user(client, get_user_from_database):
     assert data_from_resp["name"] == user_data["name"]
     assert data_from_resp["surname"] == user_data["surname"]
     assert data_from_resp["email"] == user_data["email"]
-    # users_from_db = get_user_from_database(user_data["user_id"])
-    # assert len(users_from_db) == 1
-    # user_from_db = dict[users_from_db[0]]
-    # assert user_from_db["name"] == user_data["name"]
-    # assert user_from_db["surname"] == user_data["surname"]
-    # assert user_from_db["email"] == user_data["email"]
-    # assert user_from_db["is_activa"] == True
-    # assert str(user_from_db["user_id"]) == user_data["user_id"]
+    users_from_db = await get_user_from_database(data_from_resp["user_id"])
+    assert len(users_from_db) == 1
+    user_from_db = dict(users_from_db[0])
+    assert user_from_db["name"] == user_data["name"]
+    assert user_from_db["surname"] == user_data["surname"]
+    assert user_from_db["email"] == user_data["email"]
+    assert user_from_db["is_active"] == True
+    assert str(user_from_db["user_id"]) == data_from_resp["user_id"]
