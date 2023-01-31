ea20c8ad422210487e169be76383f73e6363fb34
cherrydan <cherrydan.1978@gmail.com>
2023-01-31 20:11:06 +0300

    Using pytest-asyncio==0.18.3 in requi

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,21 @@
-#############################################################
-#                   FIXTURES FOR OUR TESTS                  #
-#############################################################
+import asyncio
+import os
+from typing import Any
+from typing import Generator
 
-from typing import Generator, Any
+import asyncpg
 import pytest
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from starlette.testclient import TestClient
-from settings import TEST_DATABASE_URL
-from main import app
-import os
-import asyncio
+
+import settings
 from db.session import get_db
-import asyncpg
-
-test_engine = create_async_engine(TEST_DATABASE_URL, future=True, echo=True)
-
-test_async_session = sessionmaker(test_engine, expire_on_commit=False, class_=AsyncSession)
+from main import app
 
 CLEAN_TABLES = [
-    'users',
+    "users",
 ]
 
 
@@ -32,43 +28,60 @@ def event_loop():
 
 @pytest.fixture(scope="session", autouse=True)
 async def run_migrations():
-    os.system('alembic init migrations')
-    os.system('alembic revision  --autogenerate -m "test running migrations"')
-    os.system('alembic upgrade heads')
+    os.system("alembic init migrations")
+    os.system('alembic revision --autogenerate -m "test running migrations"')
+    os.system("alembic upgrade heads")
 
 
 @pytest.fixture(scope="session")
-def async_session_test():
-    engine = create_async_engine(TEST_DATABASE_URL, future=True, echo=True)
-    async_session = sessionmaker(engine, expire_on_commit=True, class_=AsyncSession)
+async def async_session_test():
+    engine = create_async_engine(settings.TEST_DATABASE_URL, future=True, echo=True)
+    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
     yield async_session
 
 
 @pytest.fixture(scope="function", autouse=True)
 async def clean_tables(async_session_test):
+    """Clean data in all tables before running test function"""
     async with async_session_test() as session:
         async with session.begin():
             for table_for_cleaning in CLEAN_TABLES:
-                await session.execute(f"""TRUNCATE {table_for_cleaning};""")
+                await session.execute(f"""TRUNCATE TABLE {table_for_cleaning};""")
+
+
+async def _get_test_db():
+    try:
+        # create async engine for interaction with database
+        test_engine = create_async_engine(
+            settings.TEST_DATABASE_URL, future=True, echo=True
+        )
+
+        # create session for the interaction with database
+        test_async_session = sessionmaker(
+            test_engine, expire_on_commit=False, class_=AsyncSession
+        )
+        yield test_async_session()
+    finally:
+        pass
 
 
 @pytest.fixture(scope="function")
 async def client() -> Generator[TestClient, Any, None]:
-    async def _get_test_db():
-        try:
-            yield test_async_session()
-        finally:
-            pass
+    """
+    Create a new FastAPI TestClient that uses the `db_session` fixture to override
+    the `get_db` dependency that is injected into routes.
+    """
 
-    app.dependency_overrides[get_db] = _get_test_db()
-
+    app.dependency_overrides[get_db] = _get_test_db
     with TestClient(app) as client:
         yield client
 
 
 @pytest.fixture(scope="session")
 async def asyncpg_pool():
-    pool = await asyncpg.create_pool("".join(TEST_DATABASE_URL.split("+asyncpg")))
+    pool = await asyncpg.create_pool(
+        "".join(settings.TEST_DATABASE_URL.split("+asyncpg"))
+    )
     yield pool
     pool.close()
 
@@ -76,7 +89,9 @@ async def asyncpg_pool():
 @pytest.fixture
 async def get_user_from_database(asyncpg_pool):
     async def get_user_from_database_by_uuid(user_id: str):
-        async with asyncpg_pool.acquire() as conn:
-            return await conn.fetch("""SELECT FROM users WHERE user_id = $1;""", user_id)
+        async with asyncpg_pool.acquire() as connection:
+            return await connection.fetch(
+                """SELECT * FROM users WHERE user_id = $1;""", user_id
+            )
 
     return get_user_from_database_by_uuid
